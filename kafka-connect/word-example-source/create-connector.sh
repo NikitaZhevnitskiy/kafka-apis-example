811c5782dd53689e4ecf9955dc6bfabaade7a976
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2019-01-17 15:00:34 +0100

    rm: ibmmq-source connector example

--- kafka-connect/word-example-source/create-connector.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -s -XPOST http://localhost:8083/connectors -H 'Content-Type: application/json' -d @./config.json | jq .
--- kafka-connect/word-example-source/status.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -s http://localhost:8083/connectors/source-jms-ibmmq-word/status | jq .
